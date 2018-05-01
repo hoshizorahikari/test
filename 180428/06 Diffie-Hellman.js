59e1bc1a61fd5d78fbd769bd74c509a7a23e3c3d
hoshizorahikari <208343741@qq.com>
2018-05-01 22:36:36 +0900

    180501

--- 180428/06 Diffie-Hellman.js
+++ 180428/06 Diffie-Hellman.js
@@ -3,19 +3,19 @@ const crypto = require('crypto');
 
 
 let a = crypto.createDiffieHellman(512);
-let a_keys = a.generateKeys();
+let a_keys = a.generateKeys(); // 密钥a, 用作指数
 
-let prime = a.getPrime(); // 随机生成一个素数?
-let generator = a.getGenerator();
+let prime = a.getPrime(); // 随机生成一个素数?用于求余
+let generator = a.getGenerator(); // 底数
 
 console.log('Prime: ' + prime.toString('hex'));
 console.log('Generator: ' + generator.toString('hex'));
 
 
 let b = crypto.createDiffieHellman(prime, generator);
-let b_keys = b.generateKeys();
+let b_keys = b.generateKeys(); // 密钥b
 
-
+// 交换并生成最终密钥
 let a_secret = a.computeSecret(b_keys);
 let b_secret = b.computeSecret(a_keys);
 
